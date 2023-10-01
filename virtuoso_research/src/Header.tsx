4ba8ef9f96205afaa72fa902d73f67ddd375db03
Allen <allenye66@gmail.com>
2023-09-30 21:13:17 -0400

    header done

--- virtuoso_research/src/Header.tsx
+++ virtuoso_research/src/Header.tsx
@@ -1,11 +1,12 @@
 // src/Header.tsx
 import React from 'react';
 import Navigation from './Navigation';
+import './App.css'; // Import the CSS file
 
 const Header: React.FC = () => {
     return (
-        <header>
-            <h1>Virtuoso Research</h1>
+        <header className="header">
+            <h1 className="header-title">Virtuoso Research</h1>
             <Navigation />
         </header>
     );
--- virtuoso_research/src/Navigation.tsx
+++ virtuoso_research/src/Navigation.tsx
@@ -1,18 +1,19 @@
 // src/Navigation.tsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './App.css'; // Import the CSS file
 
 const Navigation: React.FC = () => {
     return (
         <nav>
-            <ul>
-                <li><Link to="/">About</Link></li>
-                <li><Link to="/group">Group</Link></li>
-                <li><Link to="/research">Research</Link></li>
-                <li><Link to="/publications">Publications</Link></li>
-                <li><Link to="/open-source-artifacts">Open-Source Artifacts</Link></li>
-                <li><Link to="/outreach">Outreach</Link></li>
-                <li><Link to="/curious-quantum">Curious (Quantum Computing)</Link></li>
+            <ul className="nav-list">
+                <li><Link to="/" className="nav-link">About</Link></li>
+                <li><Link to="/group" className="nav-link">Group</Link></li>
+                <li><Link to="/research" className="nav-link">Research</Link></li>
+                <li><Link to="/publications" className="nav-link">Publications</Link></li>
+                <li><Link to="/open-source-artifacts" className="nav-link">Open-Source Artifacts</Link></li>
+                <li><Link to="/outreach" className="nav-link">Outreach</Link></li>
+                <li><Link to="/curious-quantum" className="nav-link">Curious (Quantum Computing)</Link></li>
             </ul>
         </nav>
     );
