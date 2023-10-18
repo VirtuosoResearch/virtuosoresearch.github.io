3c8367e86546abd7962d6d160fcd255ce623a4cd
Allen <allenye66@gmail.com>
2023-10-18 19:32:07 -0400

    css for publications

--- virtuoso_research/src/Publications.tsx
+++ virtuoso_research/src/Publications.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import Header from './Header';
+import './Publications.css';
 
-// Fake publication data
+
 const publications = [
     {
         year: 2023,
@@ -48,49 +49,36 @@ const publications = [
 const Publications: React.FC = () => {
     const [groupByTopic, setGroupByTopic] = useState(false);
 
-    // Function to toggle between grouping by year and grouping by topic
+    //function for toggling between grouping by year and grouping by topic
     const toggleGrouping = () => {
         setGroupByTopic((prevGroupByTopic) => !prevGroupByTopic);
     };
 
-    // Extract unique topics from the publications
+    //get the unique topics from the publications
     const uniqueTopics = [...new Set(publications.map((pub) => pub.topic))];
 
     return (
         <div>
             <Header />
-            <div>
-                <button onClick={toggleGrouping}>
-                    {groupByTopic ? 'Group by Year' : 'Group by Topic'}
-                </button>
-                {groupByTopic ? (
-                    // Group by topic
-                    uniqueTopics.map((topic) => (
-                        <div key={topic}>
-                            <h2>{topic}</h2>
-                            <ul>
-                                {publications
-                                    .filter((publication) => publication.topic === topic)
-                                    .map((publication, index) => (
-                                        <li key={index}>
-                                            <h3>{publication.title}</h3>
-                                            <p>Authors: {publication.authors}</p>
-                                            <p>Publishing Site: {publication.publishingSite}</p>
-                                            <p>Date: {publication.date}</p>
-                                        </li>
-                                    ))}
-                            </ul>
-                        </div>
-                    ))
-                ) : (
-                    // Group by year
-                    <div>
-                        {Array.from(new Set(publications.map((pub) => pub.year)).values()).map((year) => (
-                            <div key={year}>
-                                <h2>{year}</h2>
+            <div className="publications-container">
+                <div className="publications-title">
+                    <h1>PUBLICATIONS:</h1>
+                    <p>Below is a comprehensive list of my publications, sorted by date. To organize the list differently, use the buttons to the right.</p>
+                </div>
+                <div className="toggle-button-container">
+                    <button onClick={toggleGrouping}>
+                        {groupByTopic ? 'Group by Year' : 'Group by Topic'}
+                    </button>
+                </div>
+                <div className="publications-list">
+                    {groupByTopic ? (
+                        // Group by topic
+                        uniqueTopics.map((topic) => (
+                            <div key={topic}>
+                                <h2>{topic}</h2>
                                 <ul>
                                     {publications
-                                        .filter((publication) => publication.year === year)
+                                        .filter((publication) => publication.topic === topic)
                                         .map((publication, index) => (
                                             <li key={index}>
                                                 <h3>{publication.title}</h3>
@@ -101,9 +89,30 @@ const Publications: React.FC = () => {
                                         ))}
                                 </ul>
                             </div>
-                        ))}
-                    </div>
-                )}
+                        ))
+                    ) : (
+                        // Group by year
+                        <div>
+                            {Array.from(new Set(publications.map((pub) => pub.year)).values()).map((year) => (
+                                <div key={year}>
+                                    <h2>{year}</h2>
+                                    <ul>
+                                        {publications
+                                            .filter((publication) => publication.year === year)
+                                            .map((publication, index) => (
+                                                <li key={index}>
+                                                    <h3>{publication.title}</h3>
+                                                    <p>Authors: {publication.authors}</p>
+                                                    <p>Publishing Site: {publication.publishingSite}</p>
+                                                    <p>Date: {publication.date}</p>
+                                                </li>
+                                            ))}
+                                    </ul>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
