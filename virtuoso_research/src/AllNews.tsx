612933e74bac81717ad3e4363c14bc41a718203a
Allen <allenye66@gmail.com>
2023-10-18 20:17:23 -0400

    Finish News

---
+++ virtuoso_research/src/AllNews.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import './AllNews.css';
+import Header from './Header';
+
+const AllNews: React.FC = () => {
+    const newsItems = [
+        {
+            date: "May 20, 2023",
+            content: "Dr. Maria Garcia awarded a prestigious fellowship to advance research in quantum computing.",
+        },
+        {
+            date: "April 5, 2023",
+            content: "Our lab collaborates with leading tech companies to develop state-of-the-art machine learning algorithms.",
+        },
+        {
+            date: "March 1, 2023",
+            content: "Prof. Hongyang R Zhang delivers keynote speech at the International Symposium on Advanced Computing.",
+        },
+        {
+            date: "January 25, 2023",
+            content: "Our research on quantum cryptography featured in top scientific journals.",
+        },
+        {
+            date: "December 10, 2022",
+            content: "Virtuoso Research hosts a successful workshop on AI ethics and responsible technology.",
+        },
+        {
+            date: "October 15, 2022",
+            content: "New partnerships established to enhance research in artificial intelligence and quantum computing.",
+        },
+        {
+            date: "September 3, 2022",
+            content: "Recent breakthrough in quantum algorithms achieved by our research team.",
+        },
+        {
+            date: "July 12, 2022",
+            content: "Virtuoso Research joins an international initiative for advancing quantum technologies.",
+        },
+        {
+            date: "June 1, 2022",
+            content: "Our lab's work on quantum machine learning recognized with a prestigious award.",
+        },
+        {
+            date: "April 20, 2022",
+            content: "Virtuoso Research welcomes a group of talented undergraduate researchers for summer internships.",
+        },
+    ];
+
+    return (
+        <div>
+            <Header/>
+            <div className="allnews-container">
+                <h2 className="centered-title">All News and Updates</h2>
+                <div className="news-list">
+                    {newsItems.map((item, index) => (
+                        <div className="news-item" key={index}>
+                            <p className="news-date">{item.date}</p>
+                            <p className="news-content">{item.content}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+        
+    );
+};
+
+export default AllNews;
--- virtuoso_research/src/News.tsx
+++ virtuoso_research/src/News.tsx
@@ -1,24 +1,23 @@
-// src/News.tsx
 import React from 'react';
 import './News.css';
 
 const News: React.FC = () => {
     return (
         <div className="news">
-            <h2>News</h2>
+            <h2>Recent Updates</h2>
             <div className="news-item">
-                <p>April, 2023</p>
+                <div className="news-date">April, 2023</div>
                 <p>Dr. Tirthak Patel successfully defended his Ph.D. thesis and graduated with his Ph.D.! He’ll be soon on his way to his faculty position at Rice University.</p>
             </div>
             <div className="news-item">
-                <p>March 1, 2023</p>
+                <div className="news-date">March, 2023</div>
                 <p>William Cutler presented SliQ at a poster session for MathWorks Day @ Northeastern</p>
             </div>
             <div className="news-item">
-                <p>February 12, 2023</p>
+                <div className="news-date">February, 2023</div>
                 <p>Daniel Silver presented his poster on Quantum Image Similarity Networks (SliQ) at AAAI-23, coauthored with Tirthak Patel, Aditya Ranjan, Harshitta Gandhi, and William Cutler</p>
             </div>
-            <p><a href="#" className="news-link">... see all News</a></p>
+            <p><a href="/allnews" className="news-link">... see all News</a></p>
         </div>
     );
 };
