dbe7d829da5e6f1d271c51f9588a78fe96ad52ad
hongyang <hongyang90@gmail.com>
2023-10-30 22:18:56 -0400

    title page

--- virtuoso_research/src/TitleAndMission.tsx
+++ virtuoso_research/src/TitleAndMission.tsx
@@ -12,19 +12,20 @@ const TitleAndMission: React.FC = () => {
                     highlightIndices.includes(index) ? (
                         <span key={index} className="bright-font">{char}</span>
                     ) : char
-                ))}.
+                ))}. To achieve this mission, our approach is two-fold:
+								<ul>
+									<li> By educating the next-generation of researchers and engineers who are passionate about technology. </li>
+									<li> By advancing the frontiers of knowledge in computer science, focusing on the foundational aspects. </li>
+								</ul>
             <p>
-                The team comprises of a diverse team of students and post graduates who are passionate about advancing technology such as the foundations of AI and ML, at the same time hoping to make a tangible impact of using these technology to support the society.
-            </p>
-            <p>
-                We are currently mainly working on two sets of projects.
+                The team comprises of a diverse team of students and post graduates who are passionate about advancing technology such as the foundations of AI and ML, at the same time hoping to make a tangible impact. We are currently mainly working on two sets of projects:
 								<ul>
 										<li>On the machine learning side, we have been working on developing multitask learning methods, thinking about transfer, fine-tuning. Recently we have been looking to generative AI such as instruction tuning in language models, and diffusion models.</li>
-										<li>On the foundations side (algorithmic and theoretical), we are interested in studying algorithms on matrix and tensor shaped data, thinking about implicit regularization. We have also been working on developing nonvacuous generalization for deep neural networks using Hessian.</li>
+										<li>On the foundations side (algorithmic and theoretical), we are interested in studying algorithms on matrix and tensor shaped data, thinking about implicit regularization, privacy. We have also been working on developing nonvacuous generalization bounds for deep neural networks using Hessian.</li>
 								</ul>
             </p>
 						<p>
-								To validate our solutions, we are particularly interested in validating solutions in social contexts. For instance, recently we've been looking to the prediction of traffic accidents using graph neural networks (by collecting a large-scale dataset of accident records and transport maps). 
+								To validate our solutions, we are particularly interested in problems and datasets that arise in social contexts (and relevant to social sciences). For instance, recently we've been looking to a problem around predicting traffic accidents using graph neural networks, by collecting a large-scale dataset of accident records and transportation maps.
             </p>
             
             <ImageSlideshow />
