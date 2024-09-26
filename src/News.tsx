078eb6959a0da8ddec7579e6d3421bca9ea9e2c9
Hongyang R. Zhang <hongyang90@gmail.com>
2024-09-25 20:31:49 -0400

    update

--- src/News.tsx
+++ src/News.tsx
@@ -6,21 +6,22 @@ const News: React.FC = () => {
     return (
         <div className="news">
             <h2>Recent Updates</h2>
-
+	        <div className="news-item">
+		        <div className="news-date">Sep, 2024</div>
+				<p> <a href="">New paper at EMNLP</a> on a scalable finetuning approach tested on a wide range of language models, as well as a <a href="http://arxiv.org/abs/2306.08553">two-point gradient estimate</a> for finding flat solutions in loss surfaces at TMLR </p>
+		</div>
+		<div className="news-item">
+		        <div className="news-date">Aug, 2024</div>
+				<p> <a href="https://dl.acm.org/doi/abs/10.1145/3637528.3671835">New paper</a> on a linearization technique for scaling up multitask learning (appeared in KDD24) </p>
+		</div>
 	          <div className="news-item">
-		          <div className="news-date">Aug, 2024</div>
-				      <p> <a href="https://dl.acm.org/doi/abs/10.1145/3637528.3671835">New paper</a> on a linearization technique for scaling up multitask learning (appeared in KDD24) </p>
-						</div>
-
-	          <div className="news-item">
-		          <div className="news-date">Aug, 2024</div>
-				      <p> Serve as a Senior PC of AAAI'25 and a reviewer of ACL Rolling Review </p>
-						</div>
-
-            <div className="news-item">
-                <div className="news-date">Aug, 2024</div>
-                <p> A new paper on <a href="https://openreview.net/forum?id=lmgf03HeqV">designing tree-structured data augmentation schemes</a> (appeared in TMLR24)! </p>
-            </div>
+		        <div className="news-date">Aug, 2024</div>
+				<p> Serve as a Senior PC of AAAI'25 and a reviewer of ACL Rolling Review </p>
+			</div>
+            	<div className="news-item">
+                	<div className="news-date">Aug, 2024</div>
+                	<p> A new paper on <a href="https://openreview.net/forum?id=lmgf03HeqV">designing tree-structured data augmentation schemes</a> (appeared in TMLR24)! </p>
+            	</div>
 
             <div className="news-item">
                 <div className="news-date">Jul, 2024</div>
