10f558edca951e50005f8be75e6df54b11a83b39
MichaelZona <2462970640@qq.com>
2025-04-15 17:53:58 -0400

    update

--- src/People.tsx
+++ src/People.tsx
@@ -120,7 +120,7 @@ const Group: React.FC = () => {
                 details: [
                     "M.S.",
                 ],
-                imageSrc: "portraits/xinwang.jpg", // Replace with actual image path
+                imageSrc: "portraits/xinwang.jpgdefault.jpg", // Replace with actual image path
                 link: "/"
             },
             {
@@ -129,7 +129,7 @@ const Group: React.FC = () => {
                 details: [
                     "B.S., Nanjing University"
                 ],
-                imageSrc: "default.jpg", // Replace with actual image path
+                imageSrc: "portraits/xinwang.jpg", // Replace with actual image path
                 link: "/"
             },
             {
